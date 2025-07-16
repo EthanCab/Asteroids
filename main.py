163f0531d6d45af3ce126e19eb7c6deae6d42f60
EthanCab <ethancaballero1998@gmail.com>
2025-07-16 20:13:09 +0200

    clock

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -12,7 +13,8 @@ def main():
 
         screen.fill((0, 0, 0))  # Negro
         pygame.display.flip()   # Actualiza la pantalla
-
+        
+        dt = clock.tick(60) / 1000  #limitacióm a 60 fps
 
 if __name__ == "__main__":
     main()
