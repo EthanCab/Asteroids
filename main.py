ad00a372ffecc2b36cd1680d02ebc90c7b0a3676
EthanCab <ethancaballero1998@gmail.com>
2025-07-20 00:48:29 +0200

    todo el jugador

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+   
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
 
         screen.fill((0, 0, 0))  # Negro
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()   # Actualiza la pantalla
         
         dt = clock.tick(60) / 1000  #limitacióm a 60 fps
